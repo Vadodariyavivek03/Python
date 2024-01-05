0bb3eb171a4ec7da247b37988990a0498a323cbc
Vivek <vivekvadodariya783@gmail.com>
2024-01-05 21:59:35 +0530

    Update Twelveth.py

--- Twelveth.py
+++ Twelveth.py
@@ -1,12 +1,19 @@
-#Assignment Operators
+#Assignment Operators  -- used to assigning values to the operands on the left-hand side.
 
 x = int(input("Enter the number1 : "))
 y = int(input("Enter the number2 : "))
 
-print("+= : " ,x = x + y)
-print("-= : " ,x = x - y)
-print("*= : " ,x = x * y)
-print("/= : " ,x = x / y)
-print("%= : " ,x = x % y)
-print("**= : " ,x = x ** y)   #Exponentiation which means perform power of any number(y) on left operand. 
-print("//= : " ,x = x // y)   #Floor digit which means it's round of float value nearest int digit. like, 18.5~18 and 18.7~19
+x += y
+# x -= y
+# x *= y
+# x /= y
+# x %= y
+# x **= y       #Exponentiation which means perform power of any number(y) on left operand. 
+# x //= y       #Floor digit which means it's round of float value nearest int digit. like, 18.5~18 and 18.7~19
+# x &= y        #Bitwise-AND
+# x |= y        #Bitwise-OR
+# x <<= y       
+# x >>= y
+# x ^= y        #Bitwise-XOR
+
+print(x)
