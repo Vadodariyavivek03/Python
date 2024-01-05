660927647e7e0407bc73f31ed9d87a44b29d03ae
Vivek <vivekvadodariya783@gmail.com>
2024-01-05 22:51:55 +0530

    Modify

---
+++ Thirteenth.py
@@ -0,0 +1,11 @@
+#Comparison(Reletional) Operators  
+
+x = int(input("Enter the number1 : "))
+y = int(input("Enter the number2 : "))
+
+print("== : " ,x == y)
+print("!= : " ,x != y)
+print("< : " ,x < y)
+print("> : " ,x > y)
+print("<= : " ,x <= y)
+print(">= : " ,x >= y)
