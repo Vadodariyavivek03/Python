1c35876b2ae41f51a931e9f3b997b551e7a1eb3c
Vivek <vivekvadodariya783@gmail.com>
2024-01-05 21:26:55 +0530

    Create Twelveth.py

---
+++ Twelveth.py
@@ -0,0 +1,12 @@
+#Assignment Operators
+
+x = int(input("Enter the number1 : "))
+y = int(input("Enter the number2 : "))
+
+print("+= : " ,x = x + y)
+print("-= : " ,x = x - y)
+print("*= : " ,x = x * y)
+print("/= : " ,x = x / y)
+print("%= : " ,x = x % y)
+print("**= : " ,x = x ** y)   #Exponentiation which means perform power of any number(y) on left operand. 
+print("//= : " ,x = x // y)   #Floor digit which means it's round of float value nearest int digit. like, 18.5~18 and 18.7~19
