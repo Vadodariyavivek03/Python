ead12dfa6978768581bc004214be704c3120fe7e
Vivek <vivekvadodariya783@gmail.com>
2024-01-05 21:24:14 +0530

    Update Eleventh.py

--- Eleventh.py
+++ Eleventh.py
@@ -1,2 +1,13 @@
 #Arithmatic Operators.
+#Simple Calculator.
 
+x = int(input("Enter the number1 : "))
+y = int(input("Enter the number2 : "))
+
+print("+ : " ,x + y)
+print("- : " ,x - y)
+print("* : " ,x * y)
+print("/ : " ,x / y)
+print("% : " ,x % y)
+print("** : " ,x ** y)   #Exponentiation which means perform power of any number(y) on left operand. 
+print("// : " ,x // y)   #Floor digit which means it's round of float value nearest int digit. like, 18.5~18 and 18.7~19
