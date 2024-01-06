1a3bf211da442670943f10a91aa0b9cf0bf373d5
Vivek <vivekvadodariya783@gmail.com>
2024-01-06 12:06:31 +0530

    Update Fourteenth.py

--- Fourteenth.py
+++ Fourteenth.py
@@ -1,11 +1,18 @@
-#Comparison(Reletional) Operators  
+#Logical Operators  
 
 x = int(input("Enter the number1 : "))
 y = int(input("Enter the number2 : "))
 
-print("== : " ,x == y)
-print("!= : " ,x != y)
-print("< : " ,x < y)
-print("> : " ,x > y)
-print("<= : " ,x <= y)
-print(">= : " ,x >= y)
+#AND - It will check both value true then it's give answer true otherwise false.
+
+# if x > 0 and y > 0:
+#     print("TRUE")
+# else:
+#     print("FALSE")
+
+#OR - It will check both value false then it's give answer false otherwise true.
+
+if x > 0 or y > 0:
+    print("TRUE")
+else:
+    print("FALSE")
