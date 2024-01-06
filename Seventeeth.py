134d7639448ef3fdf9148f354599fe8761ae3e2a
Vivek <vivekvadodariya783@gmail.com>
2024-01-06 22:44:47 +0530

    Create Seventeeth.py

---
+++ Seventeeth.py
@@ -0,0 +1,7 @@
+# Membership Operators -- Membership operators are used to test if a sequence is presented in an object.
+
+x = ['car','bus','bike','boat']
+
+print('car' in x)        #Returns True if a sequence with the specified value is present in the object.
+print('cycle' in x)
+print('bike' not in x)     #Returns True if a sequence with the specified value is not present in the object.
