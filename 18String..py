f2912373da5cbe677d889dd60a27c030c7a29b63
Vivek <vivekvadodariya783@gmail.com>
2024-01-08 12:05:45 +0530

    Update 18String..py

--- 18String..py
+++ 18String..py
@@ -1,2 +1,22 @@
-x = 'vivek'
-print(x.upper())
+#String Function
+
+x = 'HELLO,my name is VIVEK.i am from Rajkot.'
+
+#String Changecase function
+
+print(x.upper()) 
+print(x.lower())
+print(x.title())
+print(x.capitalize()) 
+
+# It will return true or false of corresponding string. 
+
+print(x.isupper()) 
+print(x.islower())
+print(x.istitle())
+
+# String Length and Count
+
+print(len(x)) 
+print(x.count('a'))
+
