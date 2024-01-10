b09d2e9eaa808c5ac0aeebc3ae4507b068542110
Vivek <vivekvadodariya783@gmail.com>
2024-01-10 21:07:25 +0530

    modify

---
+++ 21String.py
@@ -0,0 +1,14 @@
+# String-Function
+
+str = 'Hi, my name is vivek and i am from Rajkot, Gujrat, India.'
+
+print(str.find('nd'))
+
+print(str.rfind('am'))
+
+print(str.replace('India','INDIA'))
+
+print(str.index('ve'))
+
+print(str.rindex('am'))
+
---
+++ 22String.py
@@ -0,0 +1,13 @@
+# String-Function........
+
+str = 'Vivek123'
+print(str.isalnum())
+
+x = '5693'
+print(x.isnumeric())
+print(x.isalpha())
+
+y = '123.5'
+print(y.isdecimal())
+
+ 
