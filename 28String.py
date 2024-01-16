631c62ad50d2676460439957e06976f4298623f9
Vivek <vivekvadodariya783@gmail.com>
2024-01-16 19:28:37 +0530

    Update 28String.py

---
+++ 28String.py
@@ -0,0 +1,14 @@
+  # Ignore Case regular expression.
+
+import re
+
+pattern = re.compile(r'gujrat', re.IGNORECASE)
+
+str = "Hi, my name is vivek and i am from Rajkot, Gujrat, India."
+
+match = pattern.search(str)
+
+if match:
+    print("Match found:", match.group())
+else:
+    print("No match found.")
