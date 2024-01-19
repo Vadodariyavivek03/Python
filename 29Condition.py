e7da97a9c6dc6a997cefbc2dfcb89503b4fe2f15
Vivek <vivekvadodariya783@gmail.com>
2024-01-19 21:12:58 +0530

    Update 29Condition.py

---
+++ 29Condition.py
@@ -0,0 +1,11 @@
+# Comparision Operator with Conditional Statement.
+
+n1 = float(input("Enter the n1: "))
+n2 = float(input("Enter the n2: "))
+
+if n1 > n2:
+    print(f"{n1} is greater than {n2}")
+elif n1 < n2:
+    print(f"{n1} is less than {n2}")
+else:
+    print(f"{n1} is equal to {n2}")
