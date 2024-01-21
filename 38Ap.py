39ea01db93d8cf62e6a38494d4ff7077a060c12a
Vivek <vivekvadodariya783@gmail.com>
2024-01-21 23:58:12 +0530

    Update 38Ap.py

--- 38Ap.py
+++ 38Ap.py
@@ -1,13 +1,15 @@
-from datetime import datetime, timedelta
+# Datetime function : 
 
-# Create a timedelta object representing a duration of 5 days, 3 hours, and 30 minutes
-delta = timedelta(days=5, hours=3, minutes=30)
+import datetime
 
-# Use the timedelta to perform arithmetic with datetime objects
-current_datetime = datetime.now()
-future_datetime = current_datetime + delta
-past_datetime = current_datetime - delta
+date_str = "2022-01-21 12:30:00"
 
-print("Current datetime:", current_datetime)
-print("Future datetime:", future_datetime)
-print("Past datetime:", past_datetime)
+x = datetime.datetime.now()
+
+y = x.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+print(y)
+
+#---------------------------------------------------------------------------------------------------------------
+
+
